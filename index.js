05984874b446736b8b3477058f6500f9774fdc4e
Sunday <dev.nattapon.s@gmail.com>
2022-02-19 01:19:21 +0700

    ♻️ :refactor: add rpc and wss

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-var ethers = require("ethers");
-var url = "wss://wss.bitkubchain.io";
+const ethers = require("ethers");
+// var url = "wss://wss.bitkubchain.io";
+const url = "wss://wss-testnet.bitkubchain.io";
+// var url = "https://rpc.bitkubchain.io";
+const rpc = "https://rpc-testnet.bitkubchain.io";
 
-var init = function () {
-  var customWsProvider = new ethers.providers.WebSocketProvider(url);
+const init = function () {
+  const customWsProvider = new ethers.providers.WebSocketProvider(url);
 
+  //   customWsProvider.on("pending", (tx) => {
   customWsProvider.on("pending", (tx) => {
+    console.log("TX", tx);
     customWsProvider.getTransaction(tx).then(function (transaction) {
-      console.log(transaction);
+      if (transaction.to === "0x7E2a93863AABCD0087755f7a8e1929b2aA4B2Ba1") {
+        console.log("OWNER", transaction);
+      }
     });
   });
 
@@ -23,4 +30,13 @@ var init = function () {
   });
 };
 
-init();
+(async () => {
+  const provider = new ethers.providers.JsonRpcProvider(rpc);
+  const filterId = await provider.getTransaction(
+    "0x79d8f2dcea119f9427a5e45e27866f5d3f58fb6c78e8e01be8738bba95d74a66"
+  );
+
+  console.log(filterId);
+})();
+
+// init();
