6c6b3f51b5b2d83530893e635c3487f33cbae5df
Sunday <dev.nattapon.s@gmail.com>
2022-02-18 17:47:08 +0700

    🚀 update user etherjs

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const WebSocket = require("ws");
+var ethers = require("ethers");
+var url = "wss://wss.bitkubchain.io";
 
-const url = "wss://testnet.bkcscan.com/socket/websocket?locale=en&vsn=2.0.0";
+var init = function () {
+  var customWsProvider = new ethers.providers.WebSocketProvider(url);
 
-var conn = new WebSocket(url);
-conn.onopen = function () {
-  console.log("connect wss");
-  conn.send(
-    `["6","6","addresses:0x7e2a93863aabcd0087755f7a8e1929b2aa4b2ba1","phx_join",{}]`
-  );
-  //   conn.send(
-  //     `["6","14","addresses:0x7e2a93863aabcd0087755f7a8e1929b2aa4b2ba1","get_balance",{}]`
-  //   );
-  //   conn.send(
-  //     `["9","9","transactions:0x7401b84704a0e57fbc3f5d9c1ec83a2d4668d015b8e3f6b138ebdc9c3a37bb61","phx_join",{}]`
-  //   );
+  customWsProvider.on("pending", (tx) => {
+    customWsProvider.getTransaction(tx).then(function (transaction) {
+      console.log(transaction);
+    });
+  });
+
+  customWsProvider._websocket.on("error", async () => {
+    console.log(`Unable to connect to ${ep.subdomain} retrying in 3s...`);
+    setTimeout(init, 3000);
+  });
+  customWsProvider._websocket.on("close", async (code) => {
+    console.log(
+      `Connection lost with code ${code}! Attempting reconnect in 3s...`
+    );
+    customWsProvider._websocket.terminate();
+    setTimeout(init, 3000);
+  });
 };
 
-conn.onerror = function (error) {
-  console.error("webSocket error", error);
-};
-
-conn.onmessage = function (event) {
-  console.log("response", event.data);
-};
+init();
